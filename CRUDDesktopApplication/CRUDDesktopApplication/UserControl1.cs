b29bafe7dab7245341bd643156275b89e2907b88
Anwar421 <58421103+Anwar421@users.noreply.github.com>
2019-12-24 22:48:07 +0600

    Add files via upload

---
+++ CRUDDesktopApplication/CRUDDesktopApplication/UserControl1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDDesktopApplication
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStartNow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hey i'm Anwar! You are in my site. Thanks to Stay With us...");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
